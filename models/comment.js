632f6aae45ec58d401e6edfd32bea1993ce42cdf
NotSnowWhite <gflatch@att.net>
2024-10-30 00:06:59 -0500

    began comments model

---
+++ models/comment.js
@@ -0,0 +1,20 @@
+const sequelize = require("../config/connection");
+const { Model, DataTypes } = require(sequelize);
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull:false,
+            autoIncrement: true,
+        }
+    },
+    {
+        sequelize,
+        modelName: 'comment',
+        freezeTableName: true;    
+    }
+);
